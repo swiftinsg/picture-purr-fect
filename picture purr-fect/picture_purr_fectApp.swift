5482109d249b9b3176e1a4ddc094d63be297fee1
Jia Chen <github@jiachen.app>
2024-11-08 19:57:07 +0800

    Initial Commit

---
+++ picture purr-fect/picture_purr_fectApp.swift
@@ -0,0 +1,17 @@
+//
+//  picture_purr_fectApp.swift
+//  picture purr-fect
+//
+//  Created by Jia Chen Yee on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct picture_purr_fectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
